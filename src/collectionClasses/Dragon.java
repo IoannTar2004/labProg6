4de832cbca07f17020afacac58b165aa926d3d5d
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-02-10 21:24:58 +0300

    Modified Dragon.java

--- src/collectionClasses/Dragon.java
+++ src/collectionClasses/Dragon.java
@@ -1,5 +1,7 @@
 package src.collectionClasses;
 
+import java.util.Date;
+
 public class Dragon {
     private long id;
     private String name;
@@ -12,10 +14,11 @@ public class Dragon {
     private DragonCave cave;
 
     public Dragon(int id, String name, Coordinates coordinates, int age, Color color, DragonType type, DragonCharacter character,
-                  DragonCave cave) {
+                  DragonCave cave, Date date) {
         this.id = id;
         this.name = name;
         this.coordinates = coordinates;
+        this.creationDate = date;
         this.age = age;
         this.color = color;
         this.type = type;
