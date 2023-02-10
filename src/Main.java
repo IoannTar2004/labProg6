371b80a89509dd46c6baa8093f4774ffb545f9a4
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-02-10 23:21:12 +0300

    Created class-command for 'info' and modified some classes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 package src;
 
 import org.xml.sax.SAXException;
+import src.commands.*;
 import src.parser.Reader;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.Scanner;
 
 import static src.collectionClasses.CollArrayDeque.dragons;
 
@@ -12,6 +14,21 @@ public class Main {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException{
         Reader.parse("D:\\Program Files\\IntelliJ IDEA 2022.2.3\\PROJECTS\\laba5\\src\\src\\objects.xml");
         System.out.println(dragons);
+        Scanner scanner = new Scanner(System.in);
+
+        exit:
+        {
+        while (true) {
+            String command = scanner.nextLine();
+            switch (command) {
+                case "help":
+                    CommandInfo.execute();
+                    break;
+                case "exit":
+                    break exit;
+            }
+        }
+        }
     }
 }
 
