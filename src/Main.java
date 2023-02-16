c0a3f6c2983d950dd616541003d909db1dc6ed37
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-02-16 17:17:10 +0300

    Modified ScriptReader class

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException{
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         XMLReader.parse("D:\\Program Files\\IntelliJ IDEA 2022.2.3\\PROJECTS\\laba5\\src\\src\\objects.xml");
         Invoker.invoke();
     }
--- src/tools/ScriptReader.java
+++ src/tools/ScriptReader.java
@@ -1,9 +1,18 @@
 package src.tools;
 
-import java.io.File;
+import java.io.*;
 
 public class ScriptReader {
     public static void read(File file) {
-        System.out.println("yes");
+        try {
+            DataInputStream input = new DataInputStream(new BufferedInputStream(new FileInputStream(file)));
+
+            String line = input.readLine();
+            System.out.println(line);
+
+            input.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
