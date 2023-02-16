6ea8a4e96b2e934fe1b9917aad46121e01450e8b
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-02-16 17:33:05 +0300

    Improved Invoker class

--- src/tools/Invoker.java
+++ src/tools/Invoker.java
@@ -3,47 +3,47 @@ package src.tools;
 import src.commands.*;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public abstract class Invoker {
-    public static void invoke() {
+    public static void invoke(String command) {
         CommandInfo.date();
+        if (command.matches("\\s*help\\s*")) {
+            CommandHelp.execute();
+        } else if (command.matches("\\s*info\\s*")) {
+            CommandInfo.execute();
+        } else if (command.matches("\\s*show\\s*")) {
+            CommandShow.execute();
+        } else if (command.matches("\\s*add\\s*")) {
+            CommandAdd.execute();
+        } else if (command.matches("\\s*clear\\s*")) {
+            CommandClear.execute();
+        } else if (command.matches("\\s*remove_first\\s*")) {
+            CommandRemoveFirst.execute();
+        } else if (command.matches("\\s*head\\s*")) {
+            CommandHead.execute();
+        } else if (command.matches("\\s*update\\s*.*")) {
+            CommandUpdateId.preexecute(command);
+        } else if (command.matches("\\s*remove_by_id\\s*.*")) {
+            CommandRevoveById.preexecute(command);
+        } else if (command.matches("\\s*count_greater_than_age\\s*.*")) {
+            CommandCountGreater.preexecute(command);
+        } else if (command.matches("\\s*filter_by_cave\\s*.*")) {
+            CommandFilterByCave.preexecute(command);
+        } else if (command.matches("\\s*execute_script\\s*.*")) {
+            CommandExecuteScript.execute(command);
+        } else {
+            System.out.println("Такой команды нет!");
+        }
+    }
+
+    public static void invoke() {
         Scanner scanner = new Scanner(System.in);
-        exit:
-        {
-            while (true) {
-                String command = scanner.nextLine();
-                if (command.matches("\\s*help\\s*")) {
-                    CommandHelp.execute();
-                } else if (command.matches("\\s*info\\s*")) {
-                    CommandInfo.execute();
-                } else if (command.matches("\\s*exit\\s*")) {
-                    break exit;
-                } else if (command.matches("\\s*show\\s*")) {
-                    CommandShow.execute();
-                } else if (command.matches("\\s*add\\s*")) {
-                    CommandAdd.execute();
-                } else if (command.matches("\\s*clear\\s*")) {
-                    CommandClear.execute();
-                } else if (command.matches("\\s*remove_first\\s*")) {
-                    CommandRemoveFirst.execute();
-                } else if (command.matches("\\s*head\\s*")) {
-                    CommandHead.execute();
-                } else if (command.matches("\\s*update\\s*.*")) {
-                    CommandUpdateId.preexecute(command);
-                } else if (command.matches("\\s*remove_by_id\\s*.*")) {
-                    CommandRevoveById.preexecute(command);
-                } else if (command.matches("\\s*count_greater_than_age\\s*.*")) {
-                    CommandCountGreater.preexecute(command);
-                } else if (command.matches("\\s*filter_by_cave\\s*.*")) {
-                    CommandFilterByCave.preexecute(command);
-                } else if (command.matches("\\s*execute_script\\s*.*")) {
-                    CommandExecuteScript.execute(command);
-                }
-                else {
-                    System.out.println("Такой команды нет!");
-                }
+        while(true) {
+            String command = scanner.nextLine();
+            if (command.matches("\\s*exit\\s*")) {
+                break;
             }
+            Invoker.invoke(command);
         }
     }
 }
