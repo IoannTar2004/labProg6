d726df0b7fe16b211633fdc299024539ab7d51b8
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-02-18 14:32:50 +0300

    simplified CommandAdd.executewithscript()

--- src/commands/CommandAdd.java
+++ src/commands/CommandAdd.java
@@ -4,6 +4,7 @@ import src.collectionClasses.*;
 import src.tools.IdGenerator;
 import src.tools.RegexChecker;
 
+import java.lang.reflect.RecordComponent;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -105,97 +106,37 @@ public class CommandAdd {
 
     public static void executeWithScript(String name, String coordinates, String age, String color, String type, String character,
                                          String cave) {
-        Pattern pattern;
-        Matcher matcher;
         int count = 0;
-        Dragon dragon = new Dragon();
-        if (name.matches("\\s*")) {
-            return;
-        } else {
-            dragon.setName(name);
-            count++;
-        }
 
-        pattern = Pattern.compile("\\s*(-?\\d+\\s+-?\\d+)\\s*");
-        matcher = pattern.matcher(coordinates);
-        if(matcher.matches()) {
-            coordinates = matcher.group(1);
-            String[] coordinates1 = coordinates.split("\\s+");
-            dragon.setCoordinates(new Coordinates(Integer.parseInt(coordinates1[0]), Long.parseLong(coordinates1[1])));
-            count++;
-        } else {
-            return;
-        }
+        if (RegexChecker.nameCheck(name) != null) {count++;}
+        else {return;}
 
-        pattern = Pattern.compile("\\s*([1-9][0-9]*)\\s*");
-        matcher = pattern.matcher(age);
-        if (matcher.matches()){
-            age = matcher.group(1);
-            dragon.setAge(Integer.parseInt(age));
-            count++;
-        } else {
-            return;
-        }
+        Coordinates coordinates1 = RegexChecker.coordinatesCheck(coordinates);
+        if(coordinates1 != null) {count++;}
+        else {return;}
 
-        pattern = Pattern.compile("\\s*([123])\\s*");
-        matcher = pattern.matcher(color);
-        if (matcher.matches()) {
-            color = matcher.group(1);
-            switch (color) {
-                case "1" -> dragon.setColor(Color.BLACK);
-                case "2" -> dragon.setColor(Color.BLUE);
-                case "3" -> dragon.setColor(Color.YELLOW);
-            }
-            count++;
-        } else {
-            return;
-        }
+        int age1 = RegexChecker.ageChecker(age);
+        if (age1 != -1){count++;} else {return;}
 
-        pattern = Pattern.compile("\\s*([1-4])\\s*");
-        matcher = pattern.matcher(color);
-        if (matcher.matches()) {
-            type = matcher.group(1);
-            switch (type) {
-                case "1" -> dragon.setType(DragonType.WATER);
-                case "2" -> dragon.setType(DragonType.UNDERGROUND);
-                case "3" -> dragon.setType(DragonType.AIR);
-                case "4" -> dragon.setType(DragonType.FIRE);
-            }
-            count++;
-        } else {
-            return;
-        }
+        Color color1 = Color.getColorByNumber(color);
+        if (color1 != null){count++;} else {return;}
 
-        pattern = Pattern.compile("\\s*([123])\\s*");
-        matcher = pattern.matcher(color);
-        if(matcher.matches()) {
-            character = matcher.group(1);
-            switch (character) {
-                case "1" -> dragon.setCharacter(DragonCharacter.CUNNING);
-                case "2" -> dragon.setCharacter(DragonCharacter.EVIL);
-                case "3" -> dragon.setCharacter(DragonCharacter.CHAOTIC);
-            }
-            count++;
-        } else {
-            return;
-        }
+        DragonType type1 = DragonType.getTypeByNumber(type);
+        if (type1 != null){count++;} else {return;}
 
-        pattern = Pattern.compile("\\s*(-?\\d+\\.\\d+)\\s*");
-        matcher = pattern.matcher(cave);
-        if(matcher.matches()) {
-            cave = matcher.group(1);
-            dragon.setCave(new DragonCave(Double.parseDouble(cave)));
-            count++;
-        } else {
-            return;
-        }
+        DragonCharacter character1 = DragonCharacter.getCharacterByNumber(character);
+        if (character1 != null){count++;} else {return;}
 
-        dragon.setId(IdGenerator.generate());
+        DragonCave cave1 = RegexChecker.caveChecker(cave);
+        if (cave1 != null){count++;} else {return;}
 
-        //date
+        Long id = IdGenerator.generate();
+
+        //дата
         Date date = new Date();
-        dragon.setCreationDate(date);
+
         if (count == 7) {
+            Dragon dragon = new Dragon(id, name, coordinates1, age1, color1, type1, character1, cave1, date);
             CollectionManager.add(dragon);
         }
     }
