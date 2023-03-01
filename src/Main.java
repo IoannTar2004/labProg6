63a24e6f070bd7b96aa9877d636f6695c096dac2
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-03-01 20:50:48 +0300

    Some changes in javadoc text

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ package src;
 import src.tools.*;
 
 /**
+ * Main class runs two methods: {@link ProgramStart#start()} - explains basic things of this program and requests initial xml file
+ * and {@link Invoker#invoke()} which captures entered command and checks it using RegEx.
  * @author Ivan Tarasov
  * @since 1.8
  */
