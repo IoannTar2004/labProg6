1138e92a47fac76193ced3b9f6fa7d18cb7689d0
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-03-09 22:06:58 +0300

    Deleted commented code

--- src/tools/Invoker.java
+++ src/tools/Invoker.java
@@ -51,42 +51,6 @@ public abstract class Invoker {
             } catch (NullPointerException e) {
                 System.out.println("Такой команды нет!\n");
             }
-            /*if (input.matches("\\s*help\\s*")) {
-                CommandHelp.execute();
-            } else if (input.matches("\\s*exit\\s*")) {
-                System.exit(0);
-            } else if (input.matches("\\s*info\\s*")) {
-                CommandInfo.execute();
-            } else if (input.matches("\\s*show\\s*")) {
-                CommandShow.execute();
-            } else if (input.matches("\\s*add\\s*")) {
-                CommandAdd.execute();
-            } else if (input.matches("\\s*clear\\s*")) {
-                CommandClear.execute();
-            } else if (input.matches("\\s*remove_first\\s*")) {
-                CommandRemoveFirst.execute();
-            } else if (input.matches("\\s*head\\s*")) {
-                CommandHead.execute();
-            } else if (input.matches("\\s*update\\s*.*")) {
-                CommandUpdateId.execute(input);
-            } else if (input.matches("\\s*remove_by_id\\s*.*")) {
-                CommandRemoveById.execute(input);
-            } else if (input.matches("\\s*count_greater_than_age\\s*.*")) {
-                CommandCountGreater.preexecute(input);
-            } else if (input.matches("\\s*filter_by_cave\\s*.*")) {
-                CommandFilterByCave.preexecute(input);
-            } else if (input.matches("\\s*execute_script\\s*.*")) {
-                CommandExecuteScript.execute(input);
-            } else if (input.matches("\\s*save\\s*")) {
-                CommandSave.execute();
-            } else if (input.matches("\\s*print_descending\\s*")) {
-                CommandPrintDescending.execute();
-            } else if (input.matches("\\s*add_if_max\\s*")) {
-                CommandAddIfMax.execute();
-            }
-            else {
-                System.out.println("Такой команды нет!");
-            }*/
         }
     }
 }
