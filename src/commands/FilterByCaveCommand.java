113d5fe3fc7878a4923db02c92270206c14b1450
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-03-09 22:05:39 +0300

    Simplified execute()

--- src/commands/FilterByCaveCommand.java
+++ src/commands/FilterByCaveCommand.java
@@ -14,34 +14,6 @@ import java.util.regex.Pattern;
  */
 public class FilterByCaveCommand implements Command {
     /**
-     * Method checks if the cave depth entered correctly then runs {@link FilterByCaveCommand#execute(String...)}.
-     * If this is not the case print mistake messages.
-     * @param command command with argument as dragon's cave depth
-     */
-    public static void preexecute(String command) {
-        String cave;
-        Pattern pattern = Pattern.compile("\\s*filter_by_cave\\s*(-?(\\d+\\.\\d*)|(\\d+))\\s*");
-        Matcher matcher = pattern.matcher(command);
-
-        Pattern pattern1 = Pattern.compile("\\s*filter_by_cave\\s+(-?\\d+\\.-\\d+)\\s*");
-        Matcher matcher1 = pattern1.matcher(command);
-
-        Pattern pattern2 = Pattern.compile("\\s*filter_by_cave\\s*");
-        Matcher matcher2 = pattern2.matcher(command);
-
-        if (matcher.matches()) {
-            cave = matcher.group(1);
-            //execute(cave);
-        } else if (matcher1.matches()) {
-            System.out.println("Дробное часть числа не может быть отрицательной!");
-        } else if (matcher2.matches()) {
-            OutputText.error("NoCaveArgument");
-        } else {
-            System.out.println("Глубина пещеры - дробное число через точку!");
-        }
-    }
-
-    /**
      * Prints objects if they have a same cave depth.
      */
     @Override
