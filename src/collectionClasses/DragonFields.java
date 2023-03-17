eebf2d4afe5ff029956068fa244e059069be4ec1
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-03-17 20:49:23 +0300

    Added names to enum

--- src/collectionClasses/DragonFields.java
+++ src/collectionClasses/DragonFields.java
@@ -1,2 +1,11 @@
-package src.collectionClasses;public enum DragonFields {
+package src.collectionClasses;
+
+public enum DragonFields {
+    NAME("name"), COORDINATES("coordinates"), AGE("age"), COLOR("color"), TYPE("type"), CHARACTER("character"), CAVE("cave");
+
+    private String field;
+
+    DragonFields(String field) {
+        this.field = field;
+    }
 }
