f4fb8aca16c9896f4baf9e30141eae32ab4b40c1
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-03-22 19:59:23 +0300

    Added condition of running same scripts

--- src/commands/ExecuteScriptCommand.java
+++ src/commands/ExecuteScriptCommand.java
@@ -1,5 +1,6 @@
 package src.commands;
 
+import src.support.FileManager;
 import src.support.InputManager;
 import src.tools.Invoker;
 import src.tools.OutputText;
@@ -32,13 +33,14 @@ public class ExecuteScriptCommand implements Command{
         if (file != null) {
             try {
                 commands = ScriptReader.read(file);
-                if (commands.size() > 0) {
+                if (commands.size() > 0 && FileManager.addFileToStack(file)) {
                     int i = 0;
-                    while(!Objects.equals(commands.get(i), null)) {
-                        Invoker.invoke("script", commands.get(i), commands.get(i+1), commands.get(i+2), commands.get(i+3),
-                                commands.get(i+4), commands.get(i+5), commands.get(i+6), commands.get(i+7), commands.get(i+8));
+                    while (!Objects.equals(commands.get(i), null)) {
+                        Invoker.invoke("script", commands.get(i), commands.get(i + 1), commands.get(i + 2), commands.get(i + 3),
+                                commands.get(i + 4), commands.get(i + 5), commands.get(i + 6), commands.get(i + 7), commands.get(i + 8));
                         i++;
                     }
+                    FileManager.removeFromStack(file);
                 }
             } catch (FileNotFoundException e) {
                 OutputText.error("FileNotFound");
