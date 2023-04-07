509f3b3b9e3b4b68b6276cd483263fbe4033afd4
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-04-07 15:53:24 +0300

    CommandSender.java now forms command that will send to server

--- src/client/CommandSender.java
+++ src/client/CommandSender.java
@@ -1,28 +1,57 @@
 package src.client;
 
 import src.collections.Dragon;
+import src.server.commands.*;
 import src.tools.OutputText;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CommandSender implements Serializable {
-    private String command;
+    private static Map<String, Command> commands = new HashMap<>();
+    static {
+        commands.put("add", new AddCommand());
+        commands.put("add_if_max", new AddIfMaxCommand());
+        commands.put("help", new HelpCommand());
+        commands.put("clear", new ClearCommand());
+        commands.put("head", new HeadCommand());
+        commands.put("info", new InfoCommand());
+        commands.put("print_descending", new PrintDescendingCommand());
+        commands.put("remove_first", new RemoveFirstCommand());
+        commands.put("save", new SaveCommand());
+        commands.put("show", new ShowCommand());
+
+        commands.put("count_greater_than_age", new CountGreaterCommand());
+        commands.put("execute_script", new ExecuteScriptCommand());
+        commands.put("filter_by_cave", new FilterByCaveCommand());
+        commands.put("remove_by_id", new RemoveByIdCommand());
+        commands.put("update", new UpdateIdCommand());
+    }
+    private Command command;
+    private String commandString;
     private Dragon dragonObject;
 
-    public CommandSender(String command, Dragon dragonObject) {
-        this.command = command;
+    public CommandSender(String commandString, Dragon dragonObject) {
+        try {
+            command = commands.get(commandString);
+        } catch (NullPointerException ignored) {}
+        this.commandString = commandString;
         this.dragonObject = dragonObject;
     }
 
-    public CommandSender(String command) {
-        this.command = command;
+    public CommandSender(String commandString) {
+        try {
+            command = commands.get(commandString);
+        } catch (NullPointerException ignored) {}
+        this.commandString = commandString;
     }
 
     public String getCommand() {
-        return command;
+        return commandString;
     }
 
     public Dragon getDragonObject() {
