28349cf64758638f8e4746d0948b41ba3491e404
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-04-08 20:58:08 +0300

    Changed javadoc

--- src/server/commands/Command.java
+++ src/server/commands/Command.java
@@ -6,11 +6,10 @@ package src.server.commands;
 public interface Command {
     /**
      *
-     * @param mode - have two modes: user and script. "User" is switched on when you write down the command to terminal, "script" -
+     * @param mode have two modes: user and script. "User" is switched on when you write down the command to terminal, "script" -
      *             when commands are executed from script files.
-     * @param command - entered or read command (with arguments if they are required)
-     * @param args - arguments are supplied to execute() only if the mode is "script" and command requires extra inputs (for
-     *             example, add, update, print_descending).
+     * @param command entered or read command (with arguments if they are required)
+     * @param args arguments are supplied to execute(). Contains some special objects.
      */
     Object[] execute(String mode, String[] command, Object... args);
 }
