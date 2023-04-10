c611c2190d76ac11dc30ee03497dfc68f4a10733
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-04-10 16:53:40 +0300

    Added class Connection.java to save host and port

---
+++ src/server/modules/Connection.java
@@ -0,0 +1,19 @@
+package src.server.modules;
+
+public class Connection {
+    private String host;
+    private int port;
+
+    public Connection(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
